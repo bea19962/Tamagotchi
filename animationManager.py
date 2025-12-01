fe0bb8605ce21b809555e8d2c52a297b1562ca81
BananaGrill <49271965+bea19962@users.noreply.github.com>
2025-12-01 18:36:04 +0000

    pseudo animation manager

---
+++ animationManager.py
@@ -0,0 +1,5 @@
+# TODO
+# Mapping actions from mechanics to animations from pet
+# action animation relationship --> 1:n
+# Randomize when they should start 
+# give scores to each animations so played with less or probability
